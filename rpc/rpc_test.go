1758f835eb135e5779fb5b33d049e091e5d22a90
Vinicius Hespanha <mail@vhespanha.dev>
2024-10-30 13:06:27 -0300

    skeleton rpc package

---
+++ rpc/rpc_test.go
@@ -0,0 +1,37 @@
+package rpc_test
+
+import (
+	"testing"
+
+	"git.sr.ht/~vhespanha/lsp/rpc"
+)
+
+type EncodingExample struct {
+	Testing bool
+}
+
+func TestEnconde(t *testing.T) {
+	expected := "Content-Lenght: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodeMessage(EncodingExample{Testing: true})
+
+	if expected != actual {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Lenght: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, content, err := rpc.DecodeMessage([]byte(incomingMessage))
+	contentLength := len(content)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLength != 15 {
+		t.Fatalf("Expected: 16, Got %d", contentLength)
+	}
+
+	if method != "hi" {
+		t.Fatalf("Expected: hi, Got: %s", method)
+	}
+}
