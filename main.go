ccb90e59d8a7fe26fa15c86faa2d8c62403668bc
Vinicius Hespanha <mail@vhespanha.dev>
2024-10-30 16:18:30 -0300

    add lsp package and start streaming TextDocumentSyn messages

--- main.go
+++ main.go
@@ -2,25 +2,54 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"os"
 
+	"git.sr.ht/~vhespanha/lsp/lsp"
 	"git.sr.ht/~vhespanha/lsp/rpc"
 )
 
 func main() {
 	logger := getLogger("/home/vinicius/projects/lsp/log.txt")
 	logger.Println("Logger started")
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
+
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(logger, msg)
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("got an error: %s", err)
+			continue
+		}
+
+		handleMessage(logger, method, contents)
 	}
 }
 
-func handleMessage(logger *log.Logger, msg any) {
-	logger.Println(msg)
+func handleMessage(logger *log.Logger, method string, contents []byte) {
+	logger.Printf("Received message with method: %s", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("couldn't parse following content: %s", err)
+		}
+		logger.Printf("Connected to: %s %s",
+			request.Params.ClientInfo.Name,
+			request.Params.ClientInfo.Version)
+
+		msg := lsp.NewInitializeResponse(request.ID)
+		reply := rpc.EncodeMessage(msg)
+
+		writer := os.Stdout
+		writer.Write([]byte(reply))
+
+		logger.Print("sent response")
+	}
 }
 
 func getLogger(filename string) *log.Logger {
